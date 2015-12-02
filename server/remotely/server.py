4c4ad3abfe73d8e7fafd49c97cb8de13e178ca76
Ivan Giuliani <giuliani.v@gmail.com>
2015-12-02 22:00:24 +0000

    Announce server on the local network

--- server/remotely/server.py
+++ server/remotely/server.py
@@ -2,10 +2,12 @@ import logging
 import sys
 import socket
 import json
+import threading
 
 from remotely import control
 
-
+DEFAULT_SERVER_IFACE = "0.0.0.0"
+DEFAULT_SERVER_PORT = 5051
 BUFFER_SIZE = 2048
 
 log = logging.getLogger("")
@@ -20,31 +22,65 @@ class InvalidMessage(Exception):
 
 
 class RemotelyServer(object):
+    ANNOUNCER_INTERVAL_SECONDS = 30
+    ANNOUNCE_OBJ = {
+        "name": "announce",
+        "v": "1.0",
+    }
+
     def __init__(self, ip_addr, port, control):
         self.ip_addr = ip_addr
         self.port = port
         self.control = control
+        self.announce_timer = None
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def announce_loop(self):
+        if self.announce_timer is not None:
+            self.announce_timer.cancel()
+
+        self.announce_timer = None
+
+        self.socket.sendto(json.dumps(self.ANNOUNCE_OBJ), ('<broadcast>', self.port))
+        self.announce_timer = threading.Timer(self.ANNOUNCER_INTERVAL_SECONDS, self.announce_loop)
+        self.announce_timer.start()
 
     def listen(self):
         self.socket.bind((self.ip_addr, self.port))
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
         log.info("Server listening on %s:%d" % (self.ip_addr, self.port))
+
+        self.announce_loop()
 
         while True:
             data, addr = self.socket.recvfrom(BUFFER_SIZE)
             log.debug("Received message (from=%s): %s" % (addr, data))
 
-            cmd = self.parse_cmd(data)
+            try:
+                cmd = self.parse_cmd(data)
+            except InvalidMessage:
+                log.warn("Received invalid command: %s" % cmd)
+                continue
+
             if not self.has_command(cmd):
                 log.warn("Command not found.")
                 continue
 
             self.execute_command(cmd)
 
+    def stop(self):
+        if self.announce_timer is not None:
+            self.announce_timer.cancel()
+
     def parse_cmd(self, data):
         try:
             cmd = json.loads(data)
         except ValueError:
+            raise InvalidMessage()
+
+        if not isinstance(cmd, dict):
             raise InvalidMessage()
 
         if not self.is_authorized(cmd):
@@ -78,14 +114,18 @@ def main(args):
     port = int(args[1])
 
     server = RemotelyServer(ip, port, control=control.LinuxControl())
-    server.listen()
+    try:
+        server.listen()
+    except KeyboardInterrupt:
+        log.info("Caught ^C, exit.")
+        server.stop()
 
     return False
 
 
 def start_default():
-    ip = "0.0.0.0"
-    port = 5051
+    ip = DEFAULT_SERVER_IFACE
+    port = DEFAULT_SERVER_PORT
     main([ip, port])
 
 
